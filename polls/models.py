b2e1d6a28be2587e7da3b3c998e01e5add0e0c1a
jebersole13 <jcebersole1@gmail.com>
2025-05-09 12:56:32 +0900

    migrations

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Question(model.Model):
+class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
     
